c7efb4db9a7b48d6c2c254af4ef6c465a00f47f8
zhongyw <zw198307@163.com>
2016-09-14 15:09:48 +0800

    init webpack

--- src/app.js
+++ src/app.js
@@ -1,2 +1,51 @@
 var snakes = require('./snake.js');
+
+
+var canvas = document.getElementById("snakeCanvas"),
+	context = canvas.getContext("2d");
+
+var Game = function(){
+
+}
+Game.prototype = {
+	start: function(){
+		this.init();
+	},
+	stop: function(){
+
+	},
+	init: function(){
+		console.log("context:",context);
+
+		var resetCanvas= function() {
+		    context.clearRect(0, 0, canvas.width, canvas.height);
+		}
+
+		context.fillStyle = '#999';
+		context.font = (canvas.height) + 'px Impact, sans-serif';
+		context.textAlign = 'center';
+		context.fillText("37", canvas.width / 2, canvas.height * 0.9);
+		resetCanvas();
+		context.fillText("5", canvas.width / 2, canvas.height * 0.9);
+	}
+}
+
+var Snake = function(){
+	this.direction = "left"
+}
+Snake.prototype = {
+	move: function(){
+
+	}
+}
+new Game().start();
+
+
+
+var requestAnimationFrame = window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame;
+
+
+
 console.log(snakes);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin'); //自动打开浏览器插件
+
 module.exports = {
+	// 配置服务器
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        contentBase: "./", //最好写上，否则报错，难道这里是一个坑？
+        host: "0.0.0.0",
+        port: 8084
+    },
 	entry: './src/app.js',
 	output: {
-		path: './dist',
+		path: path.resolve(__dirname, "dist"),
+    	publicPath: "/dist/",	
 		filename: 'app.bundle.js'
-	}
+	},
+	plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserPlugin({ url: 'http://localhost:8084' })
+    ]
 }
